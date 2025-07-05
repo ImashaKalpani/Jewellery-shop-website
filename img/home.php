80e1fdff06bd7b434ac2d2de1c493501757e8d9d
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-07-05 17:04:35 +0530

    connection.php

--- img/home.php
+++ img/home.php
@@ -12,7 +12,7 @@ include 'components/connection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <title>Green Tea - Home Page</title>
+    <title>Jewellery Shop - Home Page</title>
 </head>
 
 <body>
