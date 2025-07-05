6cb3546b257d66dce8cab7112c4378a725c2a816
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-07-05 17:35:47 +0530

    header.php

---
+++ components/header.php
@@ -0,0 +1,28 @@
+<header>
+    <div>
+        <a href="home.php" class="logo"><img src="img/logo.jpg"></a>
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="view_products.php">Products</a>
+            <a href="order.php">Orders</a>
+            <a href="about.php">About Us</a>
+            <a href="contact.php">Contact Us</a>
+        </nav>
+        <div class="icons">
+            <i class="bx bxs-user" id="user-btn"></i>
+            <a href="wishlist.php" class="cart-btn"><i class="bx bx-heart"></i><sup>0</sup></a>
+            <a href="cart.php" class="cart-btn"><i class="bx bx-cart-download"></i><sup>0</sup></a>
+            <i class="bx bx-list-plus" id="menu-btn" style="font-size: 2rem;"></i>
+        </div>
+        <div>
+            <p>Username: <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>Email: <span><?php echo $_SESSION['user_email']; ?></span></p>
+            <a href="login.php" class="btn">Login</a>
+            <a href="register.php" class="btn">Register</a>
+
+            <form method="post">
+                <button type="submit" name="logout" class="logout-btn">Log Out</button>
+            </form>
+        </div>
+    </div>
+</header>
