01783a72b779640da3c8c952c43cb7a64f466029
ImashaKalpani <kalpaniimasha76@gmail.com>
2025-07-06 14:42:41 +0530

    contact

--- contact.php
+++ contact.php
@@ -1,5 +1,90 @@
-<html>
+<?php
+    include 'components/connection.php';
+?>
+    <style type="text/css">
+        <?php include 'style.css'; ?>
+    </style>
+    <!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+        <title>jewellary shop - Contact page</title>
     <body>
+        <?php include 'components/header.php'; ?>
+        <div class ="main">
+            <div class="banner">
+                <h1>Contact Us</h1>
+            </div>
+        <div class = "title2">
+            <a href="home.php">home</a>Contact Us<span> 
+        </div>
+
+        <section class="services">
+            <div class="box-container">
+                
+                <div class="box">
+                <img src="img/icon2.png">
+                <div class="detail">
+                    <h3>great savings</h3>
+                    <p>save big every order</p>
+                </div>
+                </div>
+
+                <div class="box">
+                <img src="img/icon1.png">
+                <div class="detail">
+                    <h3>24*7 support</h3>
+                    <p>one-on-one support</p>
+                </div>
+                </div>
+
+                <div class="box">
+                <img src="img/icon0.png">
+                <div class="detail">
+                    <h3>gift vouchers</h3>
+                    <p>vouchers on every festivals</p>
+                </div>
+                </div>
+
+                    <div class="box">
+                        <img src="img/icon.png">
+                            <div class="detail">
+                            <h3>worldwide delivery</h3>
+                            <p>dropship worldwide</p>
+                            </div>
+                        </div>
+                        </div>
+                    </section>
+
+                    <section class="brand">
+                        <div class="box-container">
+                        
+                        <div class="box">
+                            <img src="img/brand (1).jpg">
+                        </div>
+                        
+                        <div class="box">
+                            <img src="img/brand (2).jpg">
+                        </div>
+                        
+                        <div class="box">
+                            <img src="img/brand (3).jpg">
+                        </div>
+                        
+                        <div class="box">
+                            <img src="img/brand (4).jpg">
+                        </div>
+                        
+                        <div class="box">
+                            <img src="img/brand (5).jpg">
+                        </div>
+
+                        </div>
+                    </section>
+
+
         <div class = "form-container">
     <form method="post">
         <div class="title">
