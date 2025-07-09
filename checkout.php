6d9bcc74e15f9e78a87b5abbb65481fd84509e66
ImashaKalpani <kalpaniimasha76@gmail.com>
2025-07-09 14:02:22 +0530

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -11,17 +11,21 @@ if (isset($_SESSION['user_id'])) {
 if (isset($_POST['logout'])) {
     session_destroy();
     header("location: login.php");
-    exit;
 }
 
 if (isset($_POST['place_order'])) {
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    $number = filter_var($_POST['number'], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
-    $address = $_POST['flat'] . ', ' . $_POST['street'] . ', ' . $_POST['city'] . ', ' . $_POST['country'] . ', ' . $_POST['pincode'];
-    $address = filter_var($address, FILTER_SANITIZE_STRING);
-    $address_type = filter_var($_POST['address_type'], FILTER_SANITIZE_STRING);
-    $method = filter_var($_POST['method'], FILTER_SANITIZE_STRING);
+        $name = $_POST['name'];
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+        $number = $_POST['number'];
+        $number = filter_var($number, FILTER_SANITIZE_STRING);
+        $email = $_POST['email'];
+        $email = filter_var($email, FILTER_SANITIZE_STRING);
+        $address = $_POST['flat'].', '.$_POST['street'].', '.$_POST['city'].', '.$_POST['country'].', '.$_POST['pincode'];
+        $address = filter_var($address, FILTER_SANITIZE_STRING);
+        $address_type = $_POST['address_type'];
+        $address_type = filter_var($address_type, FILTER_SANITIZE_STRING);
+        $method = $_POST['method'];
+        $method = filter_var($method, FILTER_SANITIZE_STRING);
 
     $verify_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
     $verify_cart->execute([$user_id]);
@@ -32,31 +36,24 @@ if (isset($_POST['place_order'])) {
 
         if ($get_product->rowCount() > 0) {
             while ($fetch_p = $get_product->fetch(PDO::FETCH_ASSOC)) {
-                $insert_order = $conn->prepare("INSERT INTO `orders`(id, user_id, name, number, email, address, address_type, method, product_id, price, qty) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-                $insert_order->execute([
-                    unique_id(), $user_id, $name, $number, $email, $address, $address_type, $method,
-                    $fetch_p['id'], $fetch_p['price'], 1
-                ]);
+                $insert_order = $conn->prepare("INSERT INTO `orders`(id, user_id, name, number, email, address, address_type, $method, product_id, price, qty) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                $insert_order->execute([unique_id(), $user_id, $name, $number, $email, $address, $address_type, $method, $fetch_p['id'], $fetch_p['price'], 1]);
                 header('location: order.php');
-                exit;
             }
         } else {
             $warning_msg[] = 'Something went wrong.';
         }
     } elseif ($verify_cart->rowCount() > 0) {
-        while ($f_cart = $verify_cart->fetch(PDO::FETCH_ASSOC)) {
-            $insert_order = $conn->prepare("INSERT INTO `orders`(id, user_id, name, number, email, address, address_type, method, product_id, price, qty) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $insert_order->execute([
-                unique_id(), $user_id, $name, $number, $email, $address, $address_type, $method,
-                $f_cart['product_id'], $f_cart['price'], $f_cart['qty']
-            ]);
+        while ($f_cart = $varify_cart->fetch(PDO::FETCH_ASSOC)) {
+            $insert_order = $conn->prepare("INSERT INTO `orders`(id, user_id, name, number, email, address, address_type, $method, product_id, price, qty) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $insert_order->execute([unique_id(), $user_id, $name, $number, $email, $address, $address_type, $method, $f_cart['product_id'], $f_cart['price'], $f_cart['qty']]);
+             header('location: order.php');
         }
 
         if ($insert_order) {
             $delete_cart_id = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
             $delete_cart_id->execute([$user_id]);
             header('location: order.php');
-            exit;
         }
     } else {
         $warning_msg[] = 'Something went wrong.';
