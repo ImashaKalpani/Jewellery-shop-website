6f9fdc0597ecf53f8ea8504dcb0cf18281caabae
BiyangaKalupahana <biyangadilmini1@gmail.com>
2025-07-18 08:26:13 +0530

    new update

--- view_page.php
+++ view_page.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['user_id'])) {
 
 if (isset($_POST['logout'])) {
     session_destroy();
-    header('location: login.php');
+    header('location: home.php');
 }
 
 // adding products in wishlist
